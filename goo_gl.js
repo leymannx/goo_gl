ec58e5fcd5810df388101eb34a25da0277bfd840
Norman Leymann <norman.leymann@affinitas.de>
2015-08-05 16:41:38 +0200

    updated JS

--- goo_gl.js
+++ goo_gl.js
@@ -5,19 +5,37 @@
  */
 Drupal.behaviors.vertical_tabs_exampleFieldsetSummaries = {
   attach: function (context) {
-    // Use the fieldset class to identify the vertical tab element
-    $('fieldset#edit-goo-gl', context).drupalSetSummary(function (context) {
-      // Depending on the checkbox status, the settings will be customized, so
-      // update the summary with the custom setting textfield string or a use a
-      // default string.
-      if ($('#edit-goo-gl-enabled', context).attr('checked')) {
-        // return Drupal.checkPlain($('#edit-goo-gl-custom-setting', context).val());
-        return Drupal.t('Enabled');
-      }
-      else {
-        return Drupal.t('Disabled');
-      }
-    });
+
+    var path = $(location).attr('pathname');
+
+    // 1. node type form
+    if (path.indexOf('admin/structure/types/manage') >= 0) {
+
+      // Use the fieldset class to identify the vertical tab element
+      $('fieldset#edit-goo-gl', context).drupalSetSummary(function (context) {
+        // Depending on the checkbox status, the settings will be customized
+        if ($('#edit-goo-gl-type', context).attr('checked')) {
+          // return Drupal.checkPlain($('#edit-goo-gl-custom-setting', context).val());
+          return Drupal.t('Enabled');
+        } else {
+          return Drupal.t('Disabled');
+        }
+      });
+
+    // 2. node add/edit form
+    } else if (path.indexOf('node/add') >= 0 || (path.indexOf('node/') >= 0 && path.indexOf('/edit') >= 0)) {
+
+      $('fieldset#edit-goo-gl', context).drupalSetSummary(function (context) {
+        var enabled = Drupal.settings.goo_gl.enabled;
+        if (enabled === true) {
+          return Drupal.t('Enabled');
+        } else {
+          return Drupal.t('Disabled');
+        }
+      });
+    
+    }
+
   }
 };
 
